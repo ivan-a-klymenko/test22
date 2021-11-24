6721d04dff6d71074ae4ee9aedc6ecb110994153
iklymenko <iklymenko@evolvtechnology.com>
2021-11-24 17:26:39 +0100

    commit 1

--- app/src/main/java/com/example/test23/LoginFragment.kt
+++ app/src/main/java/com/example/test23/LoginFragment.kt
@@ -1,18 +1,22 @@
 package com.example.test23
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.test23.reflection.Person
+import java.lang.reflect.Field
 
 /**
  * @author ivan.a.klymenko@gmail.com on 21.09.2021
  */
 private const val TAG = "LoginFragment"
 
-class LoginFragment : LogFragment() {
+class LoginFragment : Fragment() {
 
-    override fun getCurrentTag(): String = TAG
+//    override fun getCurrentTag(): String = TAG
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,11 +26,33 @@ class LoginFragment : LogFragment() {
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 //        view.postDelayed({
 //            findNavController().navigate(R.id.action_loginFragment_to_mainFragment)
 //        }, 3000)
-//    }
+        startPersonLogic()
+    }
+
+    private fun startPersonLogic() {
+        val person = Person()
+        Log.d(TAG, person.toString())
+
+        val nameField: Field = person.javaClass.getDeclaredField("name")
+        nameField.isAccessible = true
+        val name = nameField.get(person) as String
+        Log.d(TAG, "name = $name")
+        val name1 = "Umang Budhwar"
+        nameField[person] = name1
+
+        val pinCodeField = person.javaClass.getDeclaredField("pinCode")
+        pinCodeField.isAccessible = true
+        val pinCode = pinCodeField.getInt(person)
+        Log.d(TAG, "pinCode = $pinCode")
+        val pinCode1 = 411057
+        pinCodeField.setInt(person, pinCode1)
+
+        Log.d(TAG, person.toString())
+    }
 
 }
---
+++ app/src/main/java/com/example/test23/reflection/Person.java
@@ -0,0 +1,104 @@
+package com.example.test23.reflection;
+
+/**
+ * @author ivan.a.klymenko@gmail.com on 08.11.2021
+ */
+public class Person {
+
+    private String name = "John";
+    private byte age = 30;
+    private short uidNumber = 5555;
+    private int pinCode = 452002;
+    private long contactNumber = 123456789L;
+    private float height = 6.1242f;
+    private double weight = 75.2564;
+    private char gender = 'M';
+    private boolean active = true;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", uidNumber=" + uidNumber +
+                ", pinCode=" + pinCode +
+                ", contactNumber=" + contactNumber +
+                ", height=" + height +
+                ", weight=" + weight +
+                ", gender=" + gender +
+                ", active=" + active +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public byte getAge() {
+        return age;
+    }
+
+    public void setAge(byte age) {
+        this.age = age;
+    }
+
+    public short getUidNumber() {
+        return uidNumber;
+    }
+
+    public void setUidNumber(short uidNumber) {
+        this.uidNumber = uidNumber;
+    }
+
+    public int getPinCode() {
+        return pinCode;
+    }
+
+    public void setPinCode(int pinCode) {
+        this.pinCode = pinCode;
+    }
+
+    public long getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(long contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
